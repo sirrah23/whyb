dd8c0a0b1660c479b47936600eaf28003e910aed
sirrah23 <hseth93@gmail.com>
2018-07-28 12:46:28 -0400

    Add user and location data storage
    We were hardcoding some user data storage before and are now going to
    use SQLAlchemy to read and write that information.
    
    The JSON Web Token code is now going to look at the database instead of
    the previously hardcoded information.
    
    We still need to hash passwords before we store them.

--- whyb_backend/main.py
+++ whyb_backend/main.py
@@ -1,36 +1,45 @@
 from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import safe_str_cmp
 
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
 
-    def __str__(self):
-        return "User(id='%s')" % self.id
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-users = [
-    User(1, 'user1', 'abcxyz'),
-    User(2, 'user2', 'abcxyz'),
-]
 
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    locations = db.relationship('Location', backref='user', lazy=True)
+
+    def __repr__(self):
+        return "User({},{})".format(self.id, self.username)
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return "Location({},{},{},{})".format(self.id, self.name, self.latitude, self.longitude)
+
 
 def authenticate(username, password):
-    user = username_table.get(username, None)
+    user = User.query.filter_by(username=username).first()
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_table.get(user_id, None)
-
-app = Flask(__name__)
-app.debug = True
-app.config['SECRET_KEY'] = 'super-secret'
+    return User.query.filter_by(id=user_id).first()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -38,6 +47,7 @@ jwt = JWT(app, authenticate, identity)
 @jwt_required()
 def protected():
     return '%s' % current_identity
+
 
 if __name__ == '__main__':
     app.run()
